830057c4c4065c9385b01cd94d234d1408b7c9c4
jackie <jackifinity.dev@gmail.com>
2025-05-30 20:17:31 +0800

    feat: json

---
+++ json/resources/main.js
@@ -0,0 +1,61 @@
+const container = document.getElementById("jsoneditor");
+const jsonkey = "jsonv";
+const options = {
+  mode: "code",
+  modes: ["code", "form", "text", "tree", "view", "preview"], // allowed modes
+  onChange: () => {
+    try {
+      localforage.setItem(jsonkey, editor.get());
+    } catch (e) {}
+  },
+};
+
+const mode = window.location.search.substring(1);
+const editor = new JSONEditor(container, options);
+async function init() {
+  let json = "";
+  try {
+    try {
+      // 获取url后面的json字符串
+      if (!mode || mode == "") {
+        json = (await localforage.getItem(jsonkey)) || json;
+      } else if ("none" == mode) {
+        json = "";
+      } else if ("clipboard" == mode) {
+        navigator.clipboard
+          .readText()
+          .then((clipText) => editor.set(JSON.parse(clipText)))
+          .catch((err) => {
+            const msg = err + "";
+            console.error(msg);
+            if (msg.indexOf("permission") > -1) {
+              toast("bad", "缺少剪切板权限");
+            } else if (msg.indexOf("focused") > -1) {
+              toast("bad", "浏览器没有获取焦点，无法获取剪切板内容");
+            }
+          });
+        return;
+      }
+    } catch (e) {
+      json = (await localforage.getItem(jsonkey)) || json;
+    }
+  } catch (e) {}
+  if (json) {
+    editor.set(json);
+  } else {
+    editor.setText(json);
+  }
+}
+init();
+
+editor.focus();
+// 设置JSONEditor实例
+window.JSONEditorInstance = editor;
+
+//加载时设置默认字体大小
+var font = parseInt(localStorage.getItem("jsonedit_fontsize"));
+if (font < 0) {
+  font = 15;
+  localStorage.setItem("jsonedit_fontsize", font);
+}
+document.querySelector(".ace_editor").style.fontSize = font + "px";
---
+++ json/sw.js
@@ -0,0 +1,54 @@
+// sw.js
+var cacheName = "jsonedit.sunzsh";
+var cacheFiles = [
+  "./",
+  "./?clipboard",
+  "./?none",
+  "./index.html",
+  "./index.html?clipborad",
+  "./index.html?none",
+  "./resources/img/jsoneditor-icons.svg",
+  "./resources/cache.js",
+  "./resources/jsoneditor.css",
+  "./resources/jsoneditor.js",
+  "./resources/localforage.min.js",
+  "./resources/main.css",
+  "./resources/main.js",
+  "./resources/toast.min.css",
+  "./resources/toast.min.js",
+];
+
+self.addEventListener("install", function (e) {
+  var cacheOpenPromise = caches.open(cacheName).then(function (cache) {
+    return cache.addAll(cacheFiles);
+  });
+  e.waitUntil(cacheOpenPromise);
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then(function (cache) {
+        return cache || fetch(e.request);
+      })
+      .catch(function (err) {
+        console.log(err);
+        return fetch(e.request);
+      })
+  );
+});
+
+self.addEventListener("activate", function (e) {
+  var cachePromise = caches.keys().then(function (keys) {
+    return Promise.all(
+      keys.map(function (key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      })
+    );
+  });
+  e.waitUntil(cachePromise);
+  return self.clients.claim();
+});
